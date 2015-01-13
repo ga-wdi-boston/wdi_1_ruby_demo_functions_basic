a5e253408226c3e351b2957292f43f40a06e78e5
J-Weeks <weeksjasons@gmail.com>
2015-01-13 12:36:28 -0500

    Added Defs and spec tests

---
+++ spec/defs_respec_day2_test.rb
@@ -0,0 +1,17 @@
+def divide(a,b)
+  return a / b
+end
+
+def subtract(a,b)
+  return a-b
+end
+
+def multiply(a,b)
+  return a * b
+end
+
+def like_candy (a,b)
+  if a == "yes" || b == "yes"
+    return true
+  end
+end
