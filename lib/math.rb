1d4de275b9857302fe6dfdd16bff549627662d43
brandonkoo <brandonkoo555@gmail.com>
2015-01-13 12:37:45 -0500

    math method is done

--- lib/math.rb
+++ lib/math.rb
@@ -1,3 +1,14 @@
 def add(a, b)
   a + b
 end
+def subtract(a, b)
+  a - b
+end
+
+def multiply(a, b)
+  a * b
+end
+
+def divide(a, b)
+  a / b
+end
--- spec/math_spec.rb
+++ spec/math_spec.rb
@@ -15,3 +15,52 @@ RSpec.describe "#add" do
     expect(add(-10, 10)).to eq(0)
   end
 end
+
+RSpec.describe "#subtract" do
+
+  # `it` is another method. It also takes a string and a block.
+  # The string is just a human-readable description of what you're saying the `add` method will do.
+  it "subtract two numbers" do
+
+    # Below we have three expectations. We could have had more or less.
+    # You don't want to test every possibility under the sun, but testing different types of possibilities is a good idea.
+    # The syntax of these is `expect(your_method(method_args)).to eq(expected_result)`
+    # Only use `eq` for numbers and boolean values
+    expect(subtract(2, 2)).to eq(0)
+    expect(subtract(10, 2)).to eq(8)
+    expect(subtract(10, 10)).to eq(0)
+  end
+end
+
+RSpec.describe "#multiply" do
+
+  # `it` is another method. It also takes a string and a block.
+  # The string is just a human-readable description of what you're saying the `add` method will do.
+  it "multiply two numbers" do
+
+    # Below we have three expectations. We could have had more or less.
+    # You don't want to test every possibility under the sun, but testing different types of possibilities is a good idea.
+    # The syntax of these is `expect(your_method(method_args)).to eq(expected_result)`
+    # Only use `eq` for numbers and boolean values
+    expect(multiply(2, 2)).to eq(4)
+    expect(multiply(10, 2)).to eq(20)
+    expect(multiply(10, 10)).to eq(100)
+  end
+end
+
+RSpec.describe "#divide" do
+
+  # `it` is another method. It also takes a string and a block.
+  # The string is just a human-readable description of what you're saying the `add` method will do.
+  it "divide two numbers" do
+
+    # Below we have three expectations. We could have had more or less.
+    # You don't want to test every possibility under the sun, but testing different types of possibilities is a good idea.
+    # The syntax of these is `expect(your_method(method_args)).to eq(expected_result)`
+    # Only use `eq` for numbers and boolean values
+    expect(divide(2, 2)).to eq(1)
+    expect(divide(10, 2)).to eq(5)
+    expect(divide(10, 10)).to eq(1)
+  end
+end
+
