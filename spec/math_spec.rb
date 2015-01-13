bf74a1be0dea19d9b8bfe29624be52c7f9a76855
Nicole Pupillo <nicole.pupillo@gmail.com>
2015-01-13 12:30:20 -0500

    Updated math exercise

--- spec/math_spec.rb
+++ spec/math_spec.rb
@@ -15,3 +15,35 @@ RSpec.describe "#add" do
     expect(add(-10, 10)).to eq(0)
   end
 end
+
+RSpec.describe "#subtract" do
+  it "subtracts two numbers" do
+    expect(subtract(6, 4)).to eq(2)
+    expect(subtract(12, 4)).to eq(8)
+    expect(subtract(4, 2)).to eq(2)
+  end
+end
+
+RSpec.describe "#multiply" do
+  it "multiplies two numbers" do
+    expect(multiply(2, 3)).to eq(6)
+    expect(multiply(10, 3)).to eq(30)
+    expect(multiply(4, 5)).to eq(20)
+  end
+end
+
+RSpec.describe "#divide" do
+  it "divides two numbers" do
+    expect(divide(15, 3)).to eq(5)
+    expect(divide(10, 2)).to eq(5)
+    expect(divide(6, 3)).to eq(2)
+  end
+end
+
+RSpec.describe "#modulus" do
+  it "calculates modulus of two numbers" do
+    expect(mod(15, 3)).to eq(0)
+    expect(mod(10, 3)).to eq(1)
+    expect(mod(8, 3)).to eq(2)
+  end
+end
