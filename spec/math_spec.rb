8626d3c340ace5a47d946b11e292060a5e799d35
KBondanza <kelly.bondanza@gmail.com>
2015-01-13 12:46:12 -0500

    Add 3 more operations to files: subtract, multiply, division

--- spec/math_spec.rb
+++ spec/math_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "#add" do
 
   # `it` is another method. It also takes a string and a block.
   # The string is just a human-readable description of what you're saying the `add` method will do.
-  it "sums two numbers" do
+  it "add two numbers" do
 
     # Below we have three expectations. We could have had more or less.
     # You don't want to test every possibility under the sun, but testing different types of possibilities is a good idea.
@@ -13,5 +13,53 @@ RSpec.describe "#add" do
     expect(add(2, 2)).to eq(4)
     expect(add(10, 2)).to eq(12)
     expect(add(-10, 10)).to eq(0)
+  end
+end
+
+RSpec.describe "#subtract" do
+
+  # `it` is another method. It also takes a string and a block.
+  # The string is just a human-readable description of what you're saying the `add` method will do.
+  it "subtract two numbers" do
+
+    # Below we have three expectations. We could have had more or less.
+    # You don't want to test every possibility under the sun, but testing different types of possibilities is a good idea.
+    # The syntax of these is `expect(your_method(method_args)).to eq(expected_result)`
+    # Only use `eq` for numbers and boolean values
+    expect(subtract(2, 2)).to eq(0)
+    expect(subtract(10, 2)).to eq(8)
+    expect(subtract(-10, 10)).to eq(-20)
+  end
+end
+
+RSpec.describe "#multiply" do
+
+  # `it` is another method. It also takes a string and a block.
+  # The string is just a human-readable description of what you're saying the `add` method will do.
+  it "multiply two numbers" do
+
+    # Below we have three expectations. We could have had more or less.
+    # You don't want to test every possibility under the sun, but testing different types of possibilities is a good idea.
+    # The syntax of these is `expect(your_method(method_args)).to eq(expected_result)`
+    # Only use `eq` for numbers and boolean values
+    expect(multiply(2, 2)).to eq(4)
+    expect(multiply(10, 2)).to eq(20)
+    expect(multiply(-10, 10)).to eq(-100)
+  end
+end
+
+RSpec.describe "division" do
+
+  # `it` is another method. It also takes a string and a block.
+  # The string is just a human-readable description of what you're saying the `add` method will do.
+  it "division" do
+
+    # Below we have three expectations. We could have had more or less.
+    # You don't want to test every possibility under the sun, but testing different types of possibilities is a good idea.
+    # The syntax of these is `expect(your_method(method_args)).to eq(expected_result)`
+    # Only use `eq` for numbers and boolean values
+    expect(division(10, 5)).to eq(2)
+    expect(division(4, 2)).to eq(2)
+    expect(division(8, 4)).to eq(2)
   end
 end
