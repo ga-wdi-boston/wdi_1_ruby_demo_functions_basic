64a9407217ce05a38924f6685e66953ae80fcb81
Helen Thi <ht5658@gmail.com>
2015-01-13 12:44:58 -0500

    Math operators and RSpec tests

--- lib/math.rb
+++ lib/math.rb
@@ -1,3 +1,19 @@
 def add(a, b)
   a + b
 end
+
+def difference(a, b)
+  a - b
+end
+
+def product(a, b)
+  a * b
+end
+
+def absolute_value(a)
+  a.abs
+end
+
+def square(a)
+  a ** 2
+end
--- spec/math_spec.rb
+++ spec/math_spec.rb
@@ -1,7 +1,6 @@
 require_relative '../lib/math'
 
 RSpec.describe "#add" do
-
   # `it` is another method. It also takes a string and a block.
   # The string is just a human-readable description of what you're saying the `add` method will do.
   it "sums two numbers" do
@@ -13,5 +12,37 @@ RSpec.describe "#add" do
     expect(add(2, 2)).to eq(4)
     expect(add(10, 2)).to eq(12)
     expect(add(-10, 10)).to eq(0)
+  end
+end
+
+RSpec.describe "#difference" do
+  it "difference of two numbers" do
+    expect(difference(2, 2)).to eq(0)
+    expect(difference(10, 2)).to eq(8)
+    expect(difference(-10, 10)).to eq(-20)
+  end
+end
+
+RSpec.describe "#product" do
+  it "product of two numbers" do
+    expect(product(2, 2)).to eq(4)
+    expect(product(10, 2)).to eq(20)
+    expect(product(-10, 10)).to eq(-100)
+  end
+end
+
+RSpec.describe "#absolute_value" do
+  it "takes the absolute value of a number" do
+    expect(absolute_value(2)).to eq(2)
+    expect(absolute_value(10)).to eq(10)
+    expect(absolute_value(-10)).to eq(10)
+  end
+end
+
+RSpec.describe "#square" do
+  it "squares a number" do
+    expect(square(2)).to eq(4)
+    expect(square(10)).to eq(100)
+    expect(square(-10)).to eq(100)
   end
 end
